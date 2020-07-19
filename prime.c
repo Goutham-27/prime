ceec4bc82b57c4e1ac8fc883b5e3b54679d22dd7
Goutham-27 <67751667+Goutham-27@users.noreply.github.com>
2020-07-19 13:29:53 +0530

    created prime.c
    This code is done using recursion conception

---
+++ prime.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+ 
+int primeno(int, int);
+ int main()
+{
+    int num, check;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    check = primeno(num, num / 2);
+    if (check == 1)
+    {
+        printf("%d is a prime number\n", num);
+    }
+    else
+    {
+        printf("%d is not a prime number\n", num);
+    }
+    return 0;
+}
+ 
+int primeno(int num, int i)
+{
+    if (i == 1)
+    {
+        return 1;
+    }
+    else
+    {
+       if (num % i == 0)
+       {
+         return 0;
+       }
+       else
+       {
+         return primeno(num, i - 1);
+       }       
+    }
+}
+OUTPUT:
+Enter a number:19
+19 is a prime number
+
+Enter a number:20
+20 is not a prime number
